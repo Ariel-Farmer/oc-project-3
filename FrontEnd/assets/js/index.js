474e8c556961c5adf90ee5c222a229b28a2fc9cd
Ariel Farmer <ariel5608F@gmail.com>
2025-03-13 10:20:22 -0500

    rendered works to page from API

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -1,1 +1,32 @@
 console.log("Hello World");
+async function fetchData() {
+	try {
+		const response = await fetch("http://localhost:5678/api/works");
+
+		const data = await response.json();
+		console.log(data);
+
+		return data;
+	} catch (error) {
+		console.log("ERROR!");
+	}
+}
+
+async function displayData() {
+	const data = await fetchData();
+	const gallery = document.getElementById("gallery");
+	data.forEach(function (work) {
+		const figure = document.createElement("figure");
+		const figcaption = document.createElement("figcaption");
+		figcaption.textContent = work.title;
+		const img = document.createElement("img");
+		img.src = work.imageUrl;
+		img.alt = work.title;
+		figure.appendChild(img);
+		figure.appendChild(figcaption);
+		gallery.appendChild(figure);
+	});
+}
+
+// run display Data
+displayData();
