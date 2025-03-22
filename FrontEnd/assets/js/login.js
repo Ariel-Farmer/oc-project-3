70615da1008709c0f0a483a0c0218a24f2ea405d
Ariel Farmer <ariel5608F@gmail.com>
2025-03-22 18:03:48 -0500

    added event to sign in button

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,40 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const form = document.querySelector("form");
+	form.addEventListener("submit", function (event) {
+		event.preventDefault();
+		loginUser();
+	});
+});
+
 async function loginUser(event) {
-    //
-    const email = document.getElementById("email").value;
+	//
+	const email = document.getElementById("email").value;
 	const password = document.getElementById("password").value;
-   
-    const loginData = {
+
+	const loginData = {
 		email: email,
 		password: password,
 	};
 
-    try {
-        // use fetch API to send Post request to log user in
+	try {
+		// use fetch API to send Post request to log user in
 		const response = await fetch("http://localhost:5678/api/users/login", {
-            method: 'POST',
-            headers: {
-                Accept:"application/json",
-                "Content-Type": "application/json",
-            }
-            body: JSON.stringify(loginData)
-        });
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(loginData),
+		});
 
 		if (!response.ok) {
 			alert("!Incorrect Login Credentials!");
 		} else {
-            const data = await response.json();
-            console.log("Response Data:", data);
-            const token = data.token;
-		
-		// localStorage.setItem("token", token);
-		// const userId = data.userId;
-		// localStorage.setItem("userId", userId);
-		// window.location.href = "index.html"; 
+			const data = await response.json();
+			console.log("Response Data:", data);
+			const token = data.token;
 
-		console.log("Login successful!", data);
-        }
-     } catch (error) {
+			localStorage.setItem("token", token);
+			const userId = data.userId;
+			localStorage.setItem("userId", userId);
+			window.location.href = "index.html";
 
+			console.log("Login successful!", data);
+		}
+	} catch (error) {
 		console.error("Login error:", error.message);
 		const errorMessageElement = document.getElementById("error-message");
 		if (errorMessageElement) {
